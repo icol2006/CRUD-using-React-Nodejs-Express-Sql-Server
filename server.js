f2de96653b7a2cc427f8d5e554a855e0e0b6ee25
maykokl <maykolxx@gmail.com>
2019-02-21 11:04:08 -0600

    v2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 app.use(bodyParser.json({limit:'5mb'}));  
 app.use(bodyParser.urlencoded({extended:true, limit:'5mb'}));
 
-
+//Controller api
 var productController = require('./Controller/ProductController')();
 app.use("/api/products", productController);
 
